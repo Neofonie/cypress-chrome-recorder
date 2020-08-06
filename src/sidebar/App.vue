15d036ae65726870e0232cf4e61c6b0f90fd4a1f
Serkan Savasal <serkan.savasal@neofonie.de>
2020-08-06 12:31:29 +0200

    fe structure

--- src/sidebar/App.vue
+++ src/sidebar/App.vue
@@ -1,18 +1,55 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="wellcome-content">
+    <div>
+      {{ wellcome }}<br>
+      <img class="neo-logo" :src="neoLogo" /><br>
+      <img class="cy-logo" :src="cyLogo" />
+      {{ recorder }}<br>
+    </div>
+    <button @click="selectedComponent = 'setNewRecord'">{{ btnStart }}</button>
+    <component :is="selectedComponent">
+    </component>
   </div>
 </template>
 
 <script>
+import neoLogo from '../assets/logos/neo-logo.png';
+import cyLogo from '../assets/logos/cypress-logo.png';
+import setNewRecord from '../records/newRecord.vue';
+import allRecordsList from '../records/recordsList.vue';
+
 export default {
   data() {
     return {
-      msg: 'Welcome!'
+      wellcome: 'Welcome to the',
+      neoLogo: neoLogo,
+      cyLogo: cyLogo,
+      recorder: 'recorder!',
+      btnStart: 'Start new record!',
+      selectedComponent: allRecordsList
     }
+  },
+  components: {
+    setNewRecord: setNewRecord,
+    allRecordsList: allRecordsList
   }
 }
 </script>
 
-<style lang="stylus">
+<style>
+  .wellcome-content {
+    text-align: center;
+  }
+
+  .neo-logo {
+    background: red;
+    padding: 2px;
+    margin-top: 10px;
+    height: 30px;
+  }
+
+  .cy-logo {
+    margin-top: 10px;
+    height: 30px;
+  }
 </style>
